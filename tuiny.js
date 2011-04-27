859bf7c2a7258790c87c9caccd49a19284425f5f
charlyisidore <charlyisidore@gmx.fr>
2011-04-27 03:22:19 -0700

    Added methods for Tuiny.Slideshow

--- tuiny.js
+++ tuiny.js
@@ -980,7 +980,7 @@ Tuiny.Slideshow = new Class(
 		Parameters:
 			options - (object) The options.
 			container - (Element) The container element.
-			images - (array) Buttons or widgets.
+			images - (array) The url or Elements of the images.
 	*/
 	initialize: function()
 	{
